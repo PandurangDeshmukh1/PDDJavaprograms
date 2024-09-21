7648ccb35c9e9c8fbe6eb4618198e34f0e7d34f4
Pandurang Deshmukh <adithyadeshmukh9@gmail.com>
2024-09-21 03:18:05 -0700

    programs upload from pc

---
+++ collection.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class collection {
+    public static void main(String[] args) {
+        ArrayList<String> al = new ArrayList<String>();
+        al.add("Ravi");
+        al.add("Vijay");
+        al.add("Ajay");
+
+        Iterator<String> itr = al.iterator(); 
+        while (itr.hasNext()) {
+            System.out.println(itr.next()); 
+        }
+       
+    
+    }
+}
